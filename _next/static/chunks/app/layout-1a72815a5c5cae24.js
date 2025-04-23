e828830faeccad6cedf3f90bdb2769948f665cb6
rohanrajpatil <rohanrpatil764@gmail.com>
2025-04-23 19:56:33 -0400

    Updates

---
+++ _next/static/chunks/app/layout-1a72815a5c5cae24.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},5356:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}},8591:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5356,23)),Promise.resolve().then(l.t.bind(l,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[444,441,684,358],()=>s(8591)),_N_E=e.O()}]);
